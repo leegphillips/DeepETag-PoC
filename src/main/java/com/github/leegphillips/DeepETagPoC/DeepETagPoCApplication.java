c4aea8925befb0236abdb98cf0e460878fda1d66
Lee Phillips <Lee.Phillips@wiggle.com>
2017-09-20 14:21:17 +0100

    Init files from Spring Initializr

---
+++ src/main/java/com/github/leegphillips/DeepETagPoC/DeepETagPoCApplication.java
@@ -0,0 +1,12 @@
+package com.github.leegphillips.DeepETagPoC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepETagPoCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeepETagPoCApplication.class, args);
+	}
+}
