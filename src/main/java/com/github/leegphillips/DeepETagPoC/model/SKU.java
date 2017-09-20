8f017a65158644469faaa9abe50fbe8f4a369c7c
leegphillips <leegphillips@yahoo.co.uk>
2017-09-20 15:32:18 +0100

    Added createSKU test

---
+++ src/main/java/com/github/leegphillips/DeepETagPoC/model/SKU.java
@@ -0,0 +1,15 @@
+package com.github.leegphillips.DeepETagPoC.model;
+
+import java.util.Date;
+
+public class SKU {
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    private Date created;
+}
