b8a323d36c327c721cb66c887597290292ce185b
leegphillips <leegphillips@yahoo.co.uk>
2017-09-21 16:55:39 +0100

    Added PATCH test and initial impl

--- src/main/java/com/github/leegphillips/DeepETagPoC/SKUController.java
+++ src/main/java/com/github/leegphillips/DeepETagPoC/SKUController.java
@@ -3,7 +3,6 @@ package com.github.leegphillips.DeepETagPoC;
 
 import com.github.leegphillips.DeepETagPoC.model.SKU;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Example;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,8 +19,14 @@ public class SKUController {
         return new ResponseEntity<>(sku, HttpStatus.CREATED);
     }
 
-    @RequestMapping(value = "sku/{id}")
+    @RequestMapping(value = "/sku/{id}")
     public SKU fetchSKU(@PathVariable String id) {
         return skuDao.get(id);
     }
+
+    @RequestMapping(value = "/sku/{id}", method = RequestMethod.PATCH)
+    public ResponseEntity<?> patchSKU(@PathVariable String id) {
+        return new ResponseEntity<>(null, HttpStatus.ACCEPTED);
+    }
+
 }
--- src/test/java/com/github/leegphillips/DeepETagPoC/RestSKUIntegrationTests.java
+++ src/test/java/com/github/leegphillips/DeepETagPoC/RestSKUIntegrationTests.java
@@ -1,19 +1,24 @@
 package com.github.leegphillips.DeepETagPoC;
 
 import com.github.leegphillips.DeepETagPoC.model.SKU;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.internal.matchers.apachecommons.ReflectionEquals;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.PostConstruct;
+import java.util.Date;
 
 import static io.github.benas.randombeans.api.EnhancedRandom.random;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
@@ -22,6 +27,12 @@ public class RestSKUIntegrationTests {
 
     @Autowired
     private TestRestTemplate restTemplate;
+
+    @PostConstruct
+    public void restTemplateCannotDoPatch() {
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+        restTemplate.getRestTemplate().setRequestFactory(requestFactory);
+    }
 
     @Test
     public void createFetchPersists() {
@@ -36,5 +47,30 @@ public class RestSKUIntegrationTests {
         assertEquals(HttpStatus.OK, responseEntity2.getStatusCode());
 
         assertEquals(created, responseEntity2.getBody());
+    }
+
+    @Test
+    public void checkPatchUpdates() {
+        SKU sku = random(SKU.class, "id");
+        ResponseEntity<SKU> responseEntity1 = restTemplate.postForEntity("/sku", sku, SKU.class);
+        assertEquals(HttpStatus.CREATED, responseEntity1.getStatusCode());
+
+        SKU created = responseEntity1.getBody();
+        assertTrue(new ReflectionEquals(created, "id").matches(sku));
+
+        created.setCreated(new Date());
+        ResponseEntity<SKU> patchResponse = doPatch("/sku/" + created.getId(), created);
+        assertEquals(HttpStatus.ACCEPTED, patchResponse.getStatusCode());
+    }
+
+    // RestTemplate cannot currently do PATCH
+    private ResponseEntity<SKU> doPatch(String path, SKU update) {
+        HttpHeaders headers = new HttpHeaders();
+        MediaType mediaType = new MediaType("application", "merge-patch+json");
+        headers.setContentType(mediaType);
+
+        HttpEntity<SKU> entity = new HttpEntity<>(update, headers);
+
+        return restTemplate.exchange(path, HttpMethod.PATCH, entity, SKU.class);
     }
 }
