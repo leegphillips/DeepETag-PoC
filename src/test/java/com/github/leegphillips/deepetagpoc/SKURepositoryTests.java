a8df2019f5c736216ad2fbab7040ece1f53103d4
leegphillips <leegphillips@yahoo.co.uk>
2017-09-22 13:18:46 +0100

    Added tests to assert data layer behaviour

---
+++ src/test/java/com/github/leegphillips/deepetagpoc/SKURepositoryTests.java
@@ -0,0 +1,45 @@
+package com.github.leegphillips.deepetagpoc;
+
+import com.github.leegphillips.deepetagpoc.model.SKU;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static io.github.benas.randombeans.api.EnhancedRandom.random;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = DeepETagPoCApplication.class)
+public class SKURepositoryTests {
+    @Autowired
+    private SKURepository skuRepository;
+
+    @Test
+    public void persistSetsID() {
+        SKU sku = random(SKU.class, "id");
+        assertNull(sku.getId());
+
+        skuRepository.save(sku);
+        assertNotNull(sku.getId());
+    }
+
+    @Test
+    public void saveUpdatesState() {
+        SKU sku = random(SKU.class, "id");
+        skuRepository.save(sku);
+
+        SKU toUpdate = random(SKU.class);
+        toUpdate.setId(sku.getId());
+        skuRepository.save(toUpdate);
+
+        SKU updated = skuRepository.findOne(Example.of(sku, ExampleMatcher.matchingAny()
+                .withMatcher("id", new ExampleMatcher.GenericPropertyMatcher())));
+        assertEquals(toUpdate, updated);
+    }
+}
